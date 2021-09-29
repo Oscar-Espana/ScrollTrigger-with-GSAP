587e71c8d0db11ec3a2b4e2646ab5a09bc286d86
Oscar España <espaaoscar@gmail.com>
2021-09-29 09:22:51 -0500

    Pin Spacing

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 gsap.registerPlugin(ScrollTrigger);
 
+function getTopPartsHeight() {
+    return (document.querySelector(".pen-top").clientHeight -22)
+}
+
 function init() {
   gsap.set(".part3", {
     y: () => {
-      return -(document.querySelector(".pen-top").clientHeight -22);
+      return -getTopPartsHeight();
     },
     scrollTrigger: {
         id: "pen-body",
         trigger: ".part3", //trigger to part3 selector
         start: "top bottom-=270p", // start the trigger 
-        end: `+=${(document.querySelector(".pen-top").clientHeight -22)}`, // end the trigger when it finish the heigth of pen-top selector
+        end: `+=${getTopPartsHeight()}`, // end the trigger when it finish the heigth of pen-top selector
         pin: true,
+        pinSpacing: false,
         markers: true //show lines to know where start and end the markers
     }
   });
