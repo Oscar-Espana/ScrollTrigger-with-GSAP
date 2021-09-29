63e9ed2e16ef672f896919b5e3d23e0e979f8937
Oscar España <espaaoscar@gmail.com>
2021-09-29 08:43:35 -0500

    feat: Function based values

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-function init(){
-    
-    // start here
-
+function init() {
+  gsap.set(".part3", {
+    y: () => {
+      return -(document.querySelector(".pen-top").clientHeight -22);
+    },
+  });
 }
 
-window.addEventListener('load', function(){
-    init();
+window.addEventListener("load", function () {
+  init();
 });
